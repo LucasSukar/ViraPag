9a62239da0322006ddcc734d9bdb6e15c87c79a6
viniciusdandrade <vaj@cesar.school>
2024-03-30 12:46:50 -0300

    Recomeçando o projeto

--- virapags/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import criar
-
-admin.site.register(criar)
--- virapags/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VirapagsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'virapags'
