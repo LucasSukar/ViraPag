2dd55f54f36c1f79f51e9d15aa5b751da742ce96
viniciusdandrade <vaj@cesar.school>
2024-04-04 21:41:31 -0300

    logo arrumado

--- ViraPag/ViraPag/settings.py
+++ ViraPag/ViraPag/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 STATIC_ROOT = os.path.join('static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'templates/media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
