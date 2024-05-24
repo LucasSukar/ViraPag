7670228f3df756db6f2c71e35ade0eada6e6aa5c
andrecastrom06 <alcms@cesar.school>
2024-05-24 09:48:20 -0300

    testes aparentemente prontos

--- cypress/e2e/biblioteca.cy.js
+++ cypress/e2e/biblioteca.cy.js
@@ -147,21 +147,4 @@ describe('test suite biblioteca', () => {
         cy.get('.col-md-8 > .btn-danger').click()
         cy.get('.modal-footer > form > .btn').click()
     })
-    
-    // SO ENQUANTO ESTÁ NO LOCAL
-
-    it('Reinicializando', () => {
-        cy.visit('/admin/');
-        cy.get('#id_username').type('admin')
-        cy.get('#id_password').type('1234')
-        cy.get('.submit-row > input').click()
-        cy.get('.model-user > th > a').click()
-        cy.get(':nth-child(1) > .action-checkbox > .action-select').click()
-        cy.get(':nth-child(2) > .action-checkbox > .action-select').click()
-        cy.get(':nth-child(3) > .action-checkbox > .action-select').click()
-        cy.get(':nth-child(4) > .action-checkbox > .action-select').click()
-        cy.get('select').select('Delete selected users')
-        cy.get('.button').click()
-        cy.get('div > [type="submit"]').click()        
-    })
   })
